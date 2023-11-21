bf85d05ad7978922bd83f41993e6cf8582a65dff
권예림 <124475716+yerim07@users.noreply.github.com>
2023-11-21 18:01:14 +0900

    node_modules_download
    node_modules_download.ver

--- src/Components/DiaryList.js
+++ src/Components/DiaryList.js
@@ -1,13 +1,11 @@
 import useFetch from "./useFetch";
 
 export default function DiaryList() {
-    
+  const OBJ_DiaryList = useFetch(
+    window.location.protocol + "//" + window.location.hostname + ":8050/Diary"
+  );
 
-    const OBJ_DiaryList = useFetch(window.location.protocol  + '//' + window.location.hostname + ':8050/Diary');
+  console.log(OBJ_DiaryList);
 
-    console.log(OBJ_DiaryList);
-
-    return <div>
-        일기목록
-    </div>
+  return <div>일 기 목 록 입 니 다 만?</div>;
 }
